9142f365d335783a04c1d313e7509df0c0241752
imrannaxir <lubjagu@gmail.com>
2023-08-07 18:04:54 +0500

    COVID_19 Tracker App With Rest Apis

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+    with TickerProviderStateMixin {
+  /*
+  
+  */
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: SafeArea(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [],
+        ),
+      ),
+    );
+  }
+}
